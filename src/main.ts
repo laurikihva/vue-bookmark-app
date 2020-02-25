51b0b94549c013b0515184c803e66795805003e0
Lauri <laurikihva@gmail.com>
2020-02-25 15:41:27 +0200

    fix build errors

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-import Toasted from 'vue-toasted';
-import VueScrollTo from 'vue-scrollto';
-
 Vue.config.productionTip = false;
 
 new Vue({
@@ -13,13 +10,3 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app');
-
-const toastOptions = {
-  position: 'top-right',
-  duration: 5000,
-  singleton: true,
-  keepOnHover: true
-};
-
-Vue.use(Toasted, toastOptions);
-Vue.use(VueScrollTo);
--- src/store/index.ts
+++ src/store/index.ts
@@ -7,8 +7,21 @@ import createPersistedState from 'vuex-persistedstate';
 
 import Bookmarks from '@/store/module/bookmarks';
 
+import Toasted from 'vue-toasted';
+import VueScrollTo from 'vue-scrollto';
+
 Vue.use(Vuex);
 Vue.use(VueAxios, axios);
+
+const toastOptions = {
+  position: 'top-right',
+  duration: 5000,
+  singleton: true,
+  keepOnHover: true
+};
+
+Vue.use(Toasted, toastOptions);
+Vue.use(VueScrollTo);
 
 const store = new Vuex.Store({
   modules: {
